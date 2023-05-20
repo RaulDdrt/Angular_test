8791243128dd46c66e572c812223d7ce1d9d78c4
Raul Diaz <rauldiazdrt@gmail.com>
2023-05-20 16:41:29 +0100

    commit final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 const routes: Routes = [
 
   {path: "home" , component: HomeComponent},
-  {path: "register" , component: RegisterComponent}
+  {path: "register" , component: RegisterComponent},
+  {path: "header" , component: HeaderComponent},
+  {path: "footer" , component: FooterComponent},
 
 ];
 
