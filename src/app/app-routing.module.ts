29e678be72b82774d3bbf22d7e59d96337d12495
Raul Diaz <rauldiazdrt@gmail.com>
2023-05-20 17:41:50 +0100

    commit final de verdad de la buena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
 
 const routes: Routes = [
 
   {path: "home" , component: HomeComponent},
   {path: "register" , component: RegisterComponent},
-  {path: "header" , component: HeaderComponent},
-  {path: "footer" , component: FooterComponent},
+
 
 ];
 
