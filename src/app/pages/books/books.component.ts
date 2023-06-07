1399b070c300cf147107a0b2ac45b710c08ecead
Raul Diaz <rauldiazdrt@gmail.com>
2023-06-07 20:00:49 +0100

    cambio en las imagenes de las cards

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -11,7 +11,7 @@ export class BooksComponent {
 
   constructor() {
     this.books = [
-      new Book(20, 1, "Cronicas de la Dragonlance: El Retorno de los Dragones", "Tapa dura", "M. Weiss y T. Hitman", 20, "https://m.media-amazon.com/images/I/71oD0ziA11L.jpg"),
+      new Book(20, 1, "Dragonlance: El Retorno de los Dragones", "Tapa dura", "M. Weiss y T. Hitman", 20, "https://m.media-amazon.com/images/I/517MG545q8L.jpg"),
       new Book(2, 2, "Uzumaki", "Tapa dura", "Junji Ito", 30, "https://m.media-amazon.com/images/I/81CeGGwz+BL._AC_UF1000,1000_QL80_.jpg"),
       new Book(2, 2, "It", "Tapa blanda", "Stephen King", 20, "https://imagessl3.casadellibro.com/a/l/t5/93/9788497593793.jpg"),
     ];
